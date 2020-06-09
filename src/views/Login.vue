a14a8bef65e4f3d965e5d62ca2b571ed348526ec
adamboov <48423629+adamboov@users.noreply.github.com>
2020-06-09 12:26:05 +0800

    登录编写

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,16 +2,74 @@
   <div>
     <el-container>
       <el-header>
-        <img class="mlogo" src="https://adamboov.com/images/favicon.jpg" alt />
+        <img class="mlogo" src="https://www.markerhub.com/dist/images/logo/markerhub-logo.png" alt />
       </el-header>
-      <el-main>Main</el-main>
+      <el-main>
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="ruleForm.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
     </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      ruleForm: {
+        username: "markerhub",
+        password: "123456"
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 15, message: "长度在 3 到 15 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 5, max: 16, message: "长度在 7 到 16 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+          this.$axios
+            .post("http://localhost:8081/login", this.ruleForm)
+            .then(res => {
+              console.log(res.headers);
+              console.log(res);
+            });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
 };
 </script>
 
@@ -51,8 +109,13 @@ body > .el-container {
   line-height: 320px;
 }
 
-.mbolog {
+.mlogo {
   height: 60%;
   margin-top: 10px;
+}
+
+.demo-ruleForm {
+  max-width: 500px;
+  margin: 0 auto;
 }
 </style>
